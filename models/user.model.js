ab806ba13c16545bb7f5af4eeb4ba8391a27fc96
mxolisi nkosi <89333972+m-nkosi@users.noreply.github.com>
2024-10-26 11:02:30 +0200

    adding the register user api

---
+++ models/user.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+
+
+const UserSchemma = mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, "Please enter an email"]
+    }, 
+
+    password: {
+        type: String,
+        required: [true, "Please provide the password"]
+    },
+
+    type: {
+        type: String,
+        required: [true, "Please provide the user type"]
+    }
+
+});
+
+// Encrpting password before saving
+UserSchemma.pre('save', async function(next) {
+    if (!this.isModified('password')){
+        next();
+    }
+    this.password = await bcrypt.hash(this.password, 10);
+})
+
+const User = mongoose.model("User", UserSchemma);
+
+module.exports = User;
